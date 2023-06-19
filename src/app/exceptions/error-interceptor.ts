632022bc4baa979a8b73d97ff7095740cf2fee16
nicoleontech <nicole.sar.93@gmail.com>
2023-06-19 13:51:16 +0300

    fixed style at create component and implemented logout when the response status code is 401

--- src/app/exceptions/error-interceptor.ts
+++ src/app/exceptions/error-interceptor.ts
@@ -1,19 +1,39 @@
-import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { ErrorHandlingService } from "./error-handling.service";
-import { Observable, catchError, throwError } from "rxjs";
-
-
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ErrorHandlingService } from './error-handling.service';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { KeycloakService } from '../auth/keycloak.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private errorHandlingService: ErrorHandlingService) { }
+  constructor(
+    private errorHandlingService: ErrorHandlingService,
+    private keycloakService: KeycloakService
+  ) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
-        return next.handle(request)
-            .pipe(catchError((error: HttpErrorResponse) => {
-                this.errorHandlingService.handleHttpError(error);
-                return throwError(() => new Error("error"));
-            }))
-    }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error) {
+          switch (error.status) {
+            case 401:
+              console.error(error.status);
+              this.keycloakService.logout();
+              break;
+            default:
+              this.errorHandlingService.handleHttpError(error);
+              console.log(error);
+              break;
+          }
+        }
+        return throwError(() => new Error('error'));
+      })
+    );
+  }
 }
